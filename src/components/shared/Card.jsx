ff00f41602e7dd390b638534fa8d8b24c4beb941
Patrick Tangney <ptangney@outlook.com>
2022-06-09 11:22:44 +0100

    default props

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,7 +1,22 @@
-function Card({children}) {
+import PropTypes from 'prop-types';
+function Card({children, reverse}) {
+  //* setup conditional class */
   return (
-    <div className='card'>{children}</div>
+    // <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    <div className="card" style={{
+      backgroundColor: reverse ? 'rgba(0,0,0,0.4' : '#ffffff',
+      color: reverse ? '#ffffff' : '#000'
+    }}>{children}</div>
   )
+}
+
+Card.defaultProps = {
+  reverse: true
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool
 }
 
 export default Card
