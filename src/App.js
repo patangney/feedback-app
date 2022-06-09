d498be07e238b7c0ee95a337db16ac5c2ba29540
Patrick Tangney <ptangney@outlook.com>
2022-06-09 15:12:21 +0100

    setup router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage';
 
 function App () {
   //eslint-disable-next-line
@@ -25,14 +27,14 @@ function App () {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className='container'>
         <FeedbackForm  handleAdd={addFeedback}/>
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
-    </>
+    </Router>
   )
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
@@ -9,20 +10,37 @@ function FeedbackList ({ feedback, handleDelete }) {
   //* Loop through items *//
   return (
     <div className='feedback-list'>
-      {feedback.map(item => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          handleDelete={handleDelete}
-        /> //** handleDelete - pass to Feedback Item props */
-      ))}
+      <AnimatePresence>
+        {feedback.map(item => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+  // ** Non animated version */
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map(item => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} /> //** handleDelete - pass to Feedback Item props */
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
-    PropTypes.shape({      
+    PropTypes.shape({
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired
     })
